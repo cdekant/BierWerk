9ee3228ef84be39d4f91b97ce80113e44802f2a9
cdekant <cdekant@googlemail.com>
2023-10-08 20:54:02 +0200

    erzeugt vollständigen LaTeX-Durchlauf mit Biber und makeindex

--- BierWerk.sh
+++ BierWerk.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
+#
 pandoc --defaults=etc/Einstellungen-LaTeX.yaml
-lualatex BierWerk.tex
+#
+# Literaturverzeichnis mit biber
+biber ./BierWerk
+# Index mit makeindex neu erzeugen
+# verwendet wird "makeidx" anstatt "imakeidx" wegen Funktionalität
+# Backlinks auf die Seiten, auf der das Stichwort erwähnt wird
+# zusammen mit Paket "hyperref"
+makeindex ./BierWerk.idx
+# Die Erzeugung des PDF-Dokuments erfolgt mit lualatex
+#lualatex BierWerk.tex
+lualatex ./BierWerk
+lualatex ./BierWerk
+
+#rm ./BierWerk.aux
+#rm ./BierWerk.bbl
+#rm ./BierWerk.blg
+#rm ./BierWerk.log
+#rm ./BierWerk.toc
+
